2a242c3695d2c8543fd59aefebcddff018a73e31
Tirasigor <31933244+Tirasigor@users.noreply.github.com>
2022-08-11 21:34:26 +0300

    Add files via upload

---
+++ js/swiper-advantages.js
@@ -0,0 +1,126 @@
+const sliderA = document.querySelector('.advantages__body');
+const slidesA = Array.from(document.querySelectorAll('.advantages__item'));
+const slidesAtext = Array.from(document.querySelectorAll('.advantages__text'));
+
+let isDraggingA = false;
+let startPosA = 0;
+let currentTranslateA = 0;
+let prevTranslateA = 0;
+let animationIdA = 0;
+
+let currentIndexA = 0;
+
+window.addEventListener('resize', init);
+
+function init(){
+	currentTranslateA = 0;
+	setsliderAPosition();
+}
+
+slidesAtext.forEach((slide, index) => {
+	const slideImage = slide.querySelector('img');
+//	slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+	slide.addEventListener('touchstart', touchStartA(index));
+	slide.addEventListener('touchend', touchEndA);
+	slide.addEventListener('touchmove', touchMoveA);
+
+	slide.addEventListener('mousedown', touchStartA(index));
+	slide.addEventListener('mouseup', touchEndA);
+	slide.addEventListener('mouseleave', touchEndA);
+	slide.addEventListener('mousemove', touchMoveA);
+})
+
+slidesA.forEach((slide, index) => {
+	const slideImage = slide.querySelector('img');
+	slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+	slide.addEventListener('touchstart', touchStartA(index));
+	slide.addEventListener('touchend', touchEndA);
+	slide.addEventListener('touchmove', touchMoveA);
+
+	slide.addEventListener('mousedown', touchStartA(index));
+	slide.addEventListener('mouseup', touchEndA);
+	slide.addEventListener('mouseleave', touchEndA);
+	slide.addEventListener('mousemove', touchMoveA);
+})
+
+window.oncontextmenu = function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+	return false;
+}
+
+function touchStartA(index) {
+	if (screen.width <= 767) {
+		return function (event) {
+			currentIndexA = index;
+			startPosA = getPositionXA(event);
+			isDraggingA = true;
+
+			animationIdA = requestAnimationFrame(animationA);
+			sliderA.classList.add('grabbing');
+		}
+	}
+}
+
+function touchEndA() {
+	if (screen.width <= 767) {
+		isDraggingA = false;
+		cancelAnimationFrame(animationIdA);
+
+		const movedBy = currentTranslateA - prevTranslateA
+
+		if (movedBy < -30 && currentIndexA < 1) {
+			currentIndexA += 1;
+		}
+
+		if (movedBy > 30 && currentIndexA > 0) {
+			currentIndexA -= 1;
+		}
+
+		setPositionByIndexA();
+
+		sliderA.classList.remove('grabbing');
+	}
+}
+
+function touchMoveA(event) {
+	if (screen.width <= 767) {
+		if (isDraggingA) {
+			const currentPosition = getPositionXA(event);
+			currentTranslateA = prevTranslateA + currentPosition - startPosA;
+		}
+	}
+}
+
+function getPositionXA(event) {
+	return event.type.includes('mouse') ? event.pageX :
+		event.touches[0].clientX;
+}
+
+function animationA() {
+	setsliderAPosition();
+	if (isDraggingA) {
+		requestAnimationFrame(animationA);
+	}
+}
+
+let isFirst = true;
+
+function setsliderAPosition() {
+	sliderA.style.transform = `translateX(${currentTranslateA}px)`;
+}
+
+function setPositionByIndexA() {
+	if (isFirst){
+		isFirst = false;
+		currentIndexA = 0;
+	} else {
+		isFirst = true;
+		currentIndexA = 1;
+	}
+	currentTranslateA = currentIndexA * -slidesA[0].offsetWidth;
+	prevTranslateA = currentTranslateA;
+	setsliderAPosition();
+}
---
+++ js/swiper.js
@@ -0,0 +1,102 @@
+const slider = document.querySelector('.tabs__row');
+const slides = Array.from(document.querySelectorAll('.tabs__column'));
+
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationId = 0;
+
+let currentIndex = 0;
+
+window.addEventListener('resize', init);
+
+function init() {
+	currentTranslate = 0;
+	setSliderPosition();
+}
+
+slides.forEach((slide, index) => {
+	const slideImage = slide.querySelector('img');
+	slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+
+	slide.addEventListener('touchstart', touchStart(index))
+	slide.addEventListener('touchend', touchEnd)
+	slide.addEventListener('touchmove', touchMove)
+
+	slide.addEventListener('mousedown', touchStart(index))
+	slide.addEventListener('mouseup', touchEnd)
+	slide.addEventListener('mouseleave', touchEnd)
+	slide.addEventListener('mousemove', touchMove)
+})
+
+window.oncontextmenu = function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+	return false;
+}
+
+function touchStart(index) {
+	if (screen.width <= 767) {
+		return function (event) {
+			currentIndex = index;
+			startPos = getPositionX(event);
+			isDragging = true;
+
+			animationId = requestAnimationFrame(animation);
+			slider.classList.add('grabbing');
+		}
+	}
+}
+
+function touchEnd() {
+	if (screen.width <= 767) {
+		isDragging = false;
+		cancelAnimationFrame(animationId);
+
+		const movedBy = currentTranslate - prevTranslate
+
+		if (movedBy < -30 && currentIndex < slides.length - 1) {
+			currentIndex += 1;
+		}
+
+		if (movedBy > 30 && currentIndex > 0) {
+			currentIndex -= 1;
+		}
+
+		setPositionByIndex();
+
+		slider.classList.remove('grabbing');
+	}
+}
+
+function touchMove(event) {
+	if (screen.width <= 767) {
+		if (isDragging) {
+			const currentPosition = getPositionX(event);
+			currentTranslate = prevTranslate + currentPosition - startPos;
+		}
+	}
+}
+
+function getPositionX(event) {
+	return event.type.includes('mouse') ? event.pageX :
+		event.touches[0].clientX;
+}
+
+function animation() {
+	setSliderPosition();
+	if (isDragging) {
+		requestAnimationFrame(animation);
+	}
+}
+
+function setSliderPosition() {
+	slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+	currentTranslate = currentIndex * -slides[0].offsetWidth;
+	prevTranslate = currentTranslate;
+	setSliderPosition();
+}
