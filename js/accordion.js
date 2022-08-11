dfb256ac0609508404eecadfcd9ba4fab227659d
Tirasigor <31933244+Tirasigor@users.noreply.github.com>
2022-08-11 14:01:05 +0300

    Delete js directory

--- js/accordion.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.faq__accordion-trigger').forEach(function (item) {
-	item.addEventListener('click', () => {
-		const parent = item.parentNode;
-		parent.classList.toggle('faq__accordion-item_active');
-	})
-}
-)
