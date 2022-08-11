ee4fab5cf06067132aaee328b0560383645d503f
Tirasigor <31933244+Tirasigor@users.noreply.github.com>
2022-08-11 13:53:59 +0300

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('.faq__accordion-trigger').forEach(function (item) {
 	item.addEventListener('click', () => {
-		console.log(1);
 		const parent = item.parentNode;
 		parent.classList.toggle('faq__accordion-item_active');
 	})
